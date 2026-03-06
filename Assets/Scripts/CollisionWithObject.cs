b7f8cc5ef00f42a8f8154a54ed426a631a358dfc
viktorwaller <viktor.waller@live.se>
2026-03-06 16:04:21 +0100

    Healthbar / falldamage / deathscreen

--- Assets/Scripts/CollisionWithObject.cs
+++ Assets/Scripts/CollisionWithObject.cs
@@ -5,33 +5,22 @@ public class CollisionWithObject : MonoBehaviour
     [SerializeField] private Transform respawnPoint;
     [SerializeField] private GameObject player;
 
+    private Health health;
+
 
     void Start()
     {
-
+        health = GetComponent<Health>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
     void OnCollisionEnter2D(Collision2D collision)
     {
 
         
         if (collision.gameObject.CompareTag("KillPlayer"))
         {
-            if (LevelStats.Instance != null)
-                LevelStats.Instance.AddDeath();
-
-            player.transform.position = respawnPoint.position;
-            Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
-            if (rb != null)
-            {
-                rb.linearVelocity = Vector2.zero;
-                rb.angularVelocity = 0f;
-            }
+            health.Killplayer();
         }
     }
 }

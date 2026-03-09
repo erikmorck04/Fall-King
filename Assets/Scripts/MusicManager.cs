3ffecdfce586c9c4cd1ab87ca1d32df9ca00ce0a
erikmorck04 <erikmorck04@gmail.com>
2026-03-09 21:03:52 +0100

    fixat musik så att den inte upprepar sig varje gång

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -2,8 +2,22 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    private static MusicManager instance;
+
     void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        // Om det inte finns någon MusicManager än...
+        if (instance == null)
+        {
+            // ...då blir det här objektet chefen!
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // Om det REDAN finns en MusicManager som lever... 
+            // ...då förstör vi det här nya objektet direkt så det inte blir dubbelmusik.
+            Destroy(gameObject);
+        }
     }
 }
